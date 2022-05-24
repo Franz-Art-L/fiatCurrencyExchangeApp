8586a86debb64e1df33ab7bb4a7aed6d986e0570
Ishoboy <francislandia@icloud.com>
2022-05-25 02:51:18 +0800

    my initial components to start my exchange app as of now only navbar and footer I am just getting started

---
+++ src/Footer.js
@@ -0,0 +1,38 @@
+const Footer = () => {
+    return (
+      <footer id="contactMe" className="footer mt-auto py-3 border-top">
+        <div className="row pt-2">
+          <div className="col-6">
+            <p className="copyright-statement text-center ms-4 pt-3">
+             © <a href="https://confident-murdock-8e5bba.netlify.app/"
+             target="_blank"
+              rel="noopener noreferrer"><em>Francis Artemio Landia </em></a>
+              <p>2022</p>
+            </p>
+          </div>
+          <div className="col-6 text-center">
+            <a
+              href="https://github.com/Franz-Art-L"
+              className="btn btn-default"
+              id="github"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>Github</span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/francis-artemio-landia-9a1375123/"
+              className="btn btn-default"
+              id="linkedin"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>Linkedin</span>
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
---
+++ src/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const Navbar = props => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+        <div className='container-fluid'>
+                <a className="navbar-brand font-weight-bolder d-flex justify-content-start" href="/">
+                    <span className="">myFiatExchange🔁</span>
+                </a>
+                  
+            <button className="custom-toggler navbar-toggler" 
+            type="button" 
+            data-toggle="collapse" data-target="#navbarsExample09" 
+            aria-controls="navbarsExample09" 
+            aria-expanded={!isNavCollapsed ? true : false} 
+            aria-label="Toggle navigation" onClick={handleNavCollapse}>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            
+                    <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarsExample09">
+                        <div className='d-flex flex-wrap'>
+                        <a className="nav-link text-info" href="#converter">Converter</a>
+                            <a className="nav-link text-info" href="#chart">Chart</a>
+                            <a className="nav-link text-info" href="#exchange-rate">Exchange Rate</a>
+                            <a className="nav-link text-info" href="#contact-me">Contact Me</a>
+                        </div>
+                    </div>
+        </div>
+    
+      
+    </nav>
+  );
+}
+
+export default Navbar;
